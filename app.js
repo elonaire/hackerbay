e67fe0d4d7589ce67857dceab0b0901344ecbe7b
elonaire <elonsantos63@gmail>
2019-01-14 09:55:59 +0300

    add cors

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const session = require('express-session');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const usersRouter = require('./api/routes/users');
-const patchRouter = require('./api/routes/jsonpatch');
-const thumbnailRouter = require('./api/routes/thumbnails');
+const usersRouter = require("./api/routes/users");
+const patchRouter = require("./api/routes/jsonpatch");
+const thumbnailRouter = require("./api/routes/thumbnails");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+
+app.use(logger("dev"));
 app.use(cookieParser());
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: "hackerbayinterview"
-}));
 
-app.use('/users', usersRouter);
-app.use('/jsonpatch', patchRouter);
-app.use('/thumbnails', thumbnailRouter);
+app.use("/users", usersRouter);
+app.use("/jsonpatch", patchRouter);
+app.use("/thumbnails", thumbnailRouter);
 
-app.use((req,res,next)=>{
-	res.status(404).json({
+app.use((req, res, next) => {
+  res.status(404).json({
     message: "Resource not found!"
   });
 });
 
-app.use((err,req,res,next)=>{
-	res.status(500).json({
+app.use((err, req, res, next) => {
+  res.status(500).json({
     message: `Internal server error=> ${err}`
   });
 });
